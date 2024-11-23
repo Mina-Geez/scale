3cc9e88c8b7f8e353454f6b0c88c721535307f3a
Mina-Geez <mina@inspect-solutions.com>
2024-11-23 18:25:03 +0200

    Delete scale_integration directory

--- scale_integration/hooks.py
+++
@@ -1,9 +0,0 @@
-
-doc_events = {
-    "POS Invoice": {
-        "before_save": "scale_integration.scale_integration.scale.process_scale_barcode"
-    }
-}
-
-
-fixtures = ["Custom Field", "Custom Script", "Scale Settings"]
--- scale_integration/scale_integration/doctype/scale_settings/scale_settings.py
+++
@@ -1,8 +0,0 @@
-
-# Scale Settings Doctype
-
-import frappe
-from frappe.model.document import Document
-
-class ScaleSettings(Document):
-    pass
