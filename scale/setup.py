b277f56eff87cb4ca8c6673c61ec7563ec247d85
Mina-Geez <mina@inspect-solutions.com>
2024-11-23 19:10:02 +0200

    .

---
+++ scale/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='scale_integration',
+    version='0.0.1',
+    description='Scale integration for Frappe/ERPNext',
+    author='ERPGulf',
+    author_email='support@erpgulf.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[]
+)
